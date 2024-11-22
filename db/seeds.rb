83ec612706e29e73067e3f90d3732cbee54bb996
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-11-22 19:08:08 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[{name: 'Shubham', email_address: 'shubham@gmail.com', password: '123456'}, {name: 'ravi', email_address: 'ravi@gmail.com', password: '123456'},
+{name: 'Vivek', email_address: 'vivek@gmail.com', password: '123456'}].each do |user_hash|
+  User.create!(user_hash)
+end
+puts "Users are created!"
+
+User.all.each do |user|
+  random_price = rand(1000..9999)
+  user.orders.create!(price: random_price)
+end
+puts "Orders created!"
